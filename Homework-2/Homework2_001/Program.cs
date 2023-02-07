ee5bb1b48493fdac0949c6e6101f21d1ff88dbb6
Andrey <Andre.68.1@mail.ru>
2023-02-07 13:48:00 +0500

    Check the days of the week

---
+++ Homework-2/Homework2_001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число обозначающее день недели. Выяснить, является ли день недели выходной.
+
+Console.Clear();
+
+Console.Write("Введите номер дня недели (от 1до 7) ");
+int Number=int.Parse(Console.ReadLine() ?? "0");
+Number--;
+
+Console.WriteLine();
+string[]array={"Понедельник","Вторник","Срела","Четверг","Пятница","Суббота","воскресенье"};
+if(Number>4) Console.WriteLine("Выходной день - "+array[Number]);
+else Console.WriteLine("Рабочий д6рь: "+array[Number]);
+
+Console.WriteLine();
+
