f4e0f28f60953d17a14df4994e9bb0fa2c136e29
Andrey <Andre.68.1@mail.ru>
2023-02-07 14:05:38 +0500

    Check square

---
+++ Homework-2/Homework2_002/Program.cs
@@ -0,0 +1,14 @@
+﻿// По двум заданным числам проверить, является ли одно квадратом другого.
+
+Console.Clear();
+
+Console.Write("Введите первое число - ");
+int Number1=int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите квадрат первого - ");
+int Number2=int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+
+if (Number2==Number1*Number1) Console.WriteLine(Number2+" является квадратом числа "+Number1);
+else Console.WriteLine(Number2+" неявляется квадратом числа "+Number1);
+Console.WriteLine();
+
