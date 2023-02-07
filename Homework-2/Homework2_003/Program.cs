12a48362ce298e3593c82d1444b7ebe4c8804cb1
Andrey <Andre.68.1@mail.ru>
2023-02-07 14:11:08 +0500

    check quarter

---
+++ Homework-2/Homework2_003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задать номер четверти, показать диапозон для возможных координат.
+
+Console.Clear();
+
+Console.Write("Введите номер четверти (от 1 до 4) - ");
+int Number=int.Parse(Console.ReadLine() ?? "0");
+string Range="" ?? "0";
+
+Console.WriteLine();
+
+if (Number==1) Range="(x>o, y>0)";
+if (Number==2) Range="(x<o, y>0)";
+if (Number==3) Range="(x<o, y<0)";
+if (Number==4) Range="(x>o, y<0)";
+
+Console.WriteLine(Number+" - четверти соответствует диапозон "+ Range);
+Console.WriteLine();
